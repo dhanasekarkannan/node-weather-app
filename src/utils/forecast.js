3b3cf0957d755fab2897b90b94a3f1e2eb00d26d
Dhanasekar <sendmailtodhana@gmail.com>
2020-08-26 10:23:05 +0530

    about and forcast inforamtion updated

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('postman-request');
 
 const forecast = (latitude, longitude, callback) => {
-    const url = "http://api.weatherstack.com/current?access_key=161837e718ece5874067b4885726d45e&query=" + latitude + "," + longitude + "&units=f";
+    const url = "http://api.weatherstack.com/current?access_key=161837e718ece5874067b4885726d45e&query=" + latitude + "," + longitude ;
     request({ url: url, json: true }, (error, response) => {
         if (error) {
             callback('Unable to connect to the service', undefined);
@@ -9,8 +9,8 @@ const forecast = (latitude, longitude, callback) => {
             callback('Unable to fetch forcast, Coordinates error', undefined);
         } else {
             const current = response.body.current;
-           
-            const forecastMsg = current.weather_descriptions + " throughout the day. Current temperature is " + current.temperature + " degree Farhanite. There is " + current.precip + "% chance of rain.";
+            console.log(response.body);
+            const forecastMsg = current.weather_descriptions + " throughout the day. Current temperature is " + current.temperature + " degree Celcius outside and it feels like " + current.feelslike + " degree Celcius and humidity is " + current.humidity + ". There is " + current.precip + "% chance of rain.";
             callback(undefined, forecastMsg);
         }
     });
